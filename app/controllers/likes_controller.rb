7de25d5629400ccaa4980959a6e5d5c41c06b4d8
youngyang <yyangag@gmail.com>
2015-05-19 04:57:41 -0700

    changed @likes to likes on index.jbuilder

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,14 @@
 class LikesController < ApplicationController
   def index
     @likes = Like.order(:created_at)
-    render :json => @likes
+    # render :json => @likes
+  end
+  def create
+    @likes = Like.new(likes_params)
+  end
+
+  private 
+  def likes_params
+    
   end
 end
