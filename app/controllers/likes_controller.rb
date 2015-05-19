0230909adf840e8e9072ca9e4aebaebbdf93158d
youngyang <yyangag@gmail.com>
2015-05-19 05:10:41 -0700

    create new ratings and post_id for the like table

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,12 +3,19 @@ class LikesController < ApplicationController
     @likes = Like.order(:created_at)
     # render :json => @likes
   end
+
   def create
-    @likes = Like.new(likes_params)
+    @like = Like.new(likes_params)
+    if @like.save
+      render :json => { message: "saved"}
+    else
+      render :json => { message: "not saved"}
+    end
   end
 
   private 
   def likes_params
-    
+    params.require(:like).permit(:rating, :post_id)
   end
+
 end
