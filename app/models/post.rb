8afa748873edee812434c975233cf4bd476121fa
youngyang <yyangag@gmail.com>
2015-05-20 02:48:42 -0700

    new migration file added for pictures to posts table

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :likes
   has_many :comments
+  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100X100>"}
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 end
